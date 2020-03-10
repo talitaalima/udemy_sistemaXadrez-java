f9c40be9a5803c4eafe93914bde8a6039f8e343a
talitaalima <talitaalima@live.com>
2020-03-10 15:31:44 -0300

    segunda aula posicao

--- src/aplication/JogoXadrez.java
+++ src/aplication/JogoXadrez.java
@@ -1,13 +1,12 @@
 package aplication;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class JogoXadrez {
 	
 	public static void main (String[] args) {
 		
-		Posicao posicao = new Posicao(3, 5);
-		System.out.println(posicao);
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 	}
 
 }
